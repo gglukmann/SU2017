b553edc27942616f5b18d3d2bf5a2777a19beb5b
Gert Glükmann <gglukmann@gmail.com>
2017-05-24 17:16:13 +0300

    gulp sass example and grid scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('serve', function() {
+gulp.task('browserSync', function() {
     browserSync.init({
         server: {
             baseDir: './src/'
         }
     });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/assets/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch('src/assets/scss/*.scss', ['sass']);
 });
